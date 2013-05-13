f043fe4e53000fae1ffa4f63a650023f7131e1e7
Mark Metson <knotwork@github>
2013-05-12 22:30:11 -0300

    Added Shattienator's Amazon instance nodes to DNS seeds.

--- src/net.cpp
+++ src/net.cpp
@@ -1145,6 +1145,14 @@ static const char *strDNSSeed[] = {
 "dvcstable03.devcoin.org",	// 108.61.57.117:52333
 "dvcstable04.devcoin.org",	// 5.9.233.104:52333
 "dvcstable05.devcoin.org",	// ??? Need this one ???
+"node01.devnode.com",	// The dvcnode.com ones
+"node02.devnode.com",	// are Amazon instances
+"node03.devnode.com",	// run by Shattienator
+"node04.devnode.com",	//
+"node05.devnode.com",	//
+"node06.devnode.com",	//
+"node07.devnode.com",	//
+"node08.devnode.com",	//
 "dvcstable01.devtome.com",	// 198.154.60.183:52333
 "dvcstable02.devtome.com",	// 198.154.60.61:52333
 "dvcstable03.devtome.com",	// 108.61.57.117:52333
